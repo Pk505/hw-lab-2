76976d084559681bb0b81125a5882d081c9a7072
Pk505 <kordonpu@mail.ru>
2023-11-22 10:34:30 +0300

    Commented hello_world.cpp and rewrote input and output

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 int main() {
-    char name[20];
-    cout<<"What is your name?\n";
-    cin>>name;
-    cout << "Hello World from" <<name;
+    char name[20]; //some comments
+    std::cout<<"What is your name?\n";
+    std::cin>>name;
+    std::cout << "Hello World from" <<name;
     return 0;
 }
