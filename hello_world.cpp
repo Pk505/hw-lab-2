ad45fb5110ed71be65a2852b9fb485731fdf6636
Pk505 <114156340+Pk505@users.noreply.github.com>
2023-11-24 19:11:32 +0300

    Update hello_world.cpp with new comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main() {
-    char name[20]; //some comments
+    char name[20]; //some comments new comments
     std::cout<<"What is your name?\n";
     std::cin>>name;
     std::cout << "Hello World from" <<name;
